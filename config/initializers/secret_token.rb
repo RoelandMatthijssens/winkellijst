7601cd53fd592b6323a737221b6a168bf23e6526
Roeland Matthijssens <fulgens.ailurus@gmail.com>
2013-01-31 12:40:21 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WinkelLijst::Application.config.secret_token = '4445d731e9c1b0d7b37b52f6d34ef9ce725b7e1d61004148b89d056fba667fe11429818c66e73912a5a3cf1b63d32c1a51b786c60e4c29436d8a042094c62b82'
